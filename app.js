dbc6de290e1c6613c0f5bd9da43adb2226850cb8
Naman Bhoj <namanbhoj99@gmail.com>
2021-07-12 19:46:55 +0530

    Mongo added

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+// MONGOOSE SETUP
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todo_DB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//VIEW ENGINE
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,10 +19,31 @@ const date = require(__dirname + "/date.js");
 // currentDate = date.toLocaleDateString("en-US");
 
 currentDate = date();
-var item = ["bUY fOOD"];
+
+//var item = ["bUY fOOD"]; // CURRENTLY USSED AS DATABASE
+
+//define the schema of the collection
+const itemSchema = new mongoose.Schema({ name: String });
+//compile the schema to make a model aka collection
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({ name: "Banana" });
+const item2 = new Item({ name: "Apple" });
+const item3 = new Item({ name: "Orange" });
+
+const defaultList = [item1, item2, item3];
+console.log(defaultList);
+
+Item.insertMany(defaultList, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Succesful insertion");
+  }
+});
 
 app.get("/", function (req, res) {
-  res.render("index.ejs", { currentDate: currentDate, item: item });
+  res.render("index.ejs", { currentDate: currentDate, item: defaultList });
 });
 
 app.post("/", function (req, res) {
