ee1a5f84538e4a3aecb00007d25253ae9be6c7b7
Naman Bhoj <namanbhoj99@gmail.com>
2021-07-13 22:13:15 +0530

    added procfile and gitignore

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
-let port = process.env.PORT;
+let port = process.env.PORT || 3000;
 
 app.listen(port, function (req, res) {
   console.log("Server listening at port " + port);
