2dcca951eeda78b4fee1c2887e3bdf0b7eda530a
Naman Bhoj <namanbhoj99@gmail.com>
2021-07-14 12:09:11 +0530

    heroku debug

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const app = require("./app.js");
 let port = process.env.PORT || 3000;
 
-app.listen(port, function (req, res) {
-  console.log("Server listening at port " + port);
-});
+app.listen(port);
